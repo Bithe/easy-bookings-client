2ff134fe4171eaf215c8eb4f421a40108ef0138f
Bithe <farzana.hossain5@gmail.com>
2024-09-25 01:46:35 +0600

    swiper slider

--- src/Components/FileUpload/FileUpload.jsx
+++ src/Components/FileUpload/FileUpload.jsx
@@ -3,24 +3,25 @@ import { useDropzone } from "react-dropzone";
 
 const FileUpload = () => {
   const [uploadedFiles, setUploadedFiles] = useState([]);
+
   const { getRootProps, getInputProps } = useDropzone({
     onDrop: (acceptedFiles) => {
-      setUploadedFiles(acceptedFiles);
+      setUploadedFiles((prevFiles) => [...prevFiles, ...acceptedFiles]);
       // Call your backend API endpoint to upload files
     },
   });
-  //TO DO : Customize and Style this Drag and Drop to Upload box as you want🧑‍💻😊
+
+  // REMOVE THE FILE
+  const handleRemoveFile = (fileName) => {
+    setUploadedFiles((prevFiles) =>
+      prevFiles.filter((file) => file.name !== fileName)
+    );
+  };
+
   return (
     <div {...getRootProps()}>
-      <input {...getInputProps()} />
-      <ul>
-        {uploadedFiles.map((file) => (
-          <li key={file.name}>{file.name}</li>
-        ))}
-      </ul>
-
       {/* DRAG AND DROP IMAGE/FILE */}
-      <div className=" pt-4">
+      <div className="pt-4">
         <label className="mb-5 block font-semibold text-[#07074D]">
           Upload File
         </label>
@@ -42,37 +43,46 @@ const FileUpload = () => {
           </label>
         </div>
 
-        <div className="rounded-md bg-[#F5F7FB] py-4 px-8">
-          <div className="flex items-center justify-between">
-            <span className="truncate pr-3 text-base font-medium text-[#07074D]">
-              banner-design.png
-            </span>
-            <button className="text-[#07074D]">
-              <svg
-                width="10"
-                height="10"
-                viewBox="0 0 10 10"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M0.279337 0.279338C0.651787 -0.0931121 1.25565 -0.0931121 1.6281 0.279338L9.72066 8.3719C10.0931 8.74435 10.0931 9.34821 9.72066 9.72066C9.34821 10.0931 8.74435 10.0931 8.3719 9.72066L0.279337 1.6281C-0.0931125 1.25565 -0.0931125 0.651788 0.279337 0.279338Z"
-                  fill="currentColor"
-                />
-                <path
-                  d="M0.279337 9.72066C-0.0931125 9.34821 -0.0931125 8.74435 0.279337 8.3719L8.3719 0.279338C8.74435 -0.0931127 9.34821 -0.0931123 9.72066 0.279338C10.0931 0.651787 10.0931 1.25565 9.72066 1.6281L1.6281 9.72066C1.25565 10.0931 0.651787 10.0931 0.279337 9.72066Z"
-                  fill="currentColor"
-                />
-              </svg>
-            </button>
+        {/* Check if there are uploaded files to display */}
+        {uploadedFiles.length > 0 && (
+          <div className="rounded-md bg-[#F5F7FB] py-4 px-8">
+            {/* List of uploaded files with remove buttons */}
+            <ul>
+              {uploadedFiles.map((file) => (
+                <li
+                  key={file.name}
+                  className="flex items-center justify-between p-2 border border-[#E0E0E0] rounded-lg mt-2"
+                >
+                  <span className="truncate pr-3 text-base text-[#07074D]">
+                    {file.name}
+                  </span>
+                  <button
+                    onClick={() => handleRemoveFile(file.name)}
+                    className="text-[#07074D]"
+                  >
+                    <svg
+                      width="10"
+                      height="10"
+                      viewBox="0 0 10 10"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M0.279337 0.279338C0.651787 -0.0931121 1.25565 -0.0931121 1.6281 0.279338L9.72066 8.3719C10.0931 8.74435 10.0931 9.34821 9.72066 9.72066C9.34821 10.0931 8.74435 10.0931 8.3719 9.72066L0.279337 1.6281C-0.0931125 1.25565 -0.0931125 0.651788 0.279337 0.279338Z"
+                        fill="currentColor"
+                      />
+                      <path
+                        d="M0.279337 9.72066C-0.0931125 9.34821 -0.0931125 8.74435 0.279337 8.3719L8.3719 0.279338C8.74435 -0.0931127 9.34821 -0.0931123 9.72066 0.279338C10.0931 0.651787 10.0931 1.25565 9.72066 1.6281L1.6281 9.72066C1.25565 10.0931 0.651787 10.0931 0.279337 9.72066Z"
+                        fill="currentColor"
+                      />
+                    </svg>
+                  </button>
+                </li>
+              ))}
+            </ul>
           </div>
-          <div className="relative mt-5 h-[6px] w-full rounded-lg bg-[#E2E5EF]">
-            <div className="absolute left-0 right-0 h-full w-[75%] rounded-lg bg-[#6A64F1]"></div>
-          </div>
-        </div>
+        )}
       </div>
-
-      {/*  */}
     </div>
   );
 };
--- src/Pages/Home/Category.jsx
+++ src/Pages/Home/Category.jsx
@@ -4,67 +4,88 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 
-import { Pagination } from "swiper/modules";
+import { Autoplay, Pagination } from "swiper/modules";
 
-import c1 from '../../assets/c1.jpg';
-import c2 from '../../assets/c2.jpg';
-import c3 from '../../assets/c3.jpg';
-import c4 from '../../assets/c4.jpg';
-import c5 from '../../assets/c5.jpg';
-import c6 from '../../assets/c6.jpg';
-import c7 from '../../assets/c7.jpg';
+import c1 from "../../assets/c1.jpg";
+import c2 from "../../assets/c2.jpg";
+import c3 from "../../assets/c3.jpg";
+import c4 from "../../assets/c4.jpg";
+import c5 from "../../assets/c5.jpg";
+import c6 from "../../assets/c6.jpg";
+import c7 from "../../assets/c7.jpg";
 import SectionTitle from "../../Components/SectionTitle/SectionTitle";
 
 const Category = () => {
   return (
     <div>
-<SectionTitle subHeading={"Once you find the perfect flight, simply select it"} headiing={"Select Your Place"}>
-    
-</SectionTitle>
+      <SectionTitle
+        subHeading={"Once you find the perfect flight, simply select it"}
+        headiing={"Select Your Place"}
+      ></SectionTitle>
 
-     <section>
-     <Swiper
-        slidesPerView={4}
-        spaceBetween={30}
-        centeredSlides={true}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[Pagination]}
-        className="mySwiper  "
-      >
-        <SwiperSlide className="pb-24 ">
+      <section>
+        <Swiper
+          speed={3000}
+          autoplay={{
+            delay: 3000, // Time delay between transitions in milliseconds
+            disableOnInteraction: false, // Autoplay won't be disabled after user interaction
+          }}
+          slidesPerView={4}
+          spaceBetween={30}
+          centeredSlides={true}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Pagination, Autoplay]}
+          className="mySwiper  "
+        >
+          <SwiperSlide className="pb-24 ">
             <img src={c1} className=" "></img>
-            <h3 className="lg:text-3xl text-base text-white font-bold lg:font-extrabold uppercase text-center lg:-mt-24 -mt-10">Beach</h3>
-        </SwiperSlide>
-        <SwiperSlide>
+            <h3 className="lg:text-3xl text-base text-white font-bold lg:font-extrabold uppercase text-center lg:-mt-24 -mt-10">
+              Beach
+            </h3>
+          </SwiperSlide>
+          <SwiperSlide>
             <img src={c2} className=""></img>
-            <h3 className="lg:text-3xl text-base text-white font-bold lg:font-extrabold uppercase text-center lg:-mt-24 -mt-10">Beach</h3>
-        </SwiperSlide>
-        <SwiperSlide>
+            <h3 className="lg:text-3xl text-base text-white font-bold lg:font-extrabold uppercase text-center lg:-mt-24 -mt-10">
+            Windmills
+            </h3>
+          </SwiperSlide>
+          <SwiperSlide>
             <img src={c3} className=""></img>
-            <h3 className="lg:text-3xl text-base text-white font-bold lg:font-extrabold uppercase text-center lg:-mt-24 -mt-10">Beach</h3>
-        </SwiperSlide>
-        <SwiperSlide>
+            <h3 className="lg:text-3xl text-base text-white font-bold lg:font-extrabold uppercase text-center lg:-mt-24 -mt-10">
+            Modern
+
+            </h3>
+          </SwiperSlide>
+          <SwiperSlide>
             <img src={c4} className=""></img>
-            <h3 className="lg:text-3xl text-base text-white font-bold lg:font-extrabold uppercase text-center lg:-mt-24 -mt-10">Beach</h3>
-        </SwiperSlide>
-        <SwiperSlide>
+            <h3 className="lg:text-3xl text-base text-white font-bold lg:font-extrabold uppercase text-center lg:-mt-24 -mt-10">
+            Countryside
+
+            </h3>
+          </SwiperSlide>
+          <SwiperSlide>
             <img src={c5} className=""></img>
-            <h3 className="lg:text-3xl text-base text-white font-bold lg:font-extrabold uppercase text-center lg:-mt-24 -mt-10">Beach</h3>
-        </SwiperSlide>
-        <SwiperSlide>
+            <h3 className="lg:text-3xl text-base text-white font-bold lg:font-extrabold uppercase text-center lg:-mt-24 -mt-10">
+            Pools
+            </h3>
+          </SwiperSlide>
+          <SwiperSlide>
             <img src={c6} className=""></img>
-            <h3 className="lg:text-3xl text-base text-white font-bold lg:font-extrabold uppercase text-center lg:-mt-24 -mt-10">Beach</h3>
-        </SwiperSlide>
-        <SwiperSlide>
+            <h3 className="lg:text-3xl text-base text-white font-bold lg:font-extrabold uppercase text-center lg:-mt-24 -mt-10">
+            Islands
+
+            </h3>
+          </SwiperSlide>
+          <SwiperSlide>
             <img src={c7} className=""></img>
-            <h3 className="lg:text-3xl text-base text-white font-bold lg:font-extrabold uppercase text-center lg:-mt-24 -mt-10">Beach</h3>
-        </SwiperSlide>
-      
-       
-      </Swiper>{" "}
-     </section>
+            <h3 className="lg:text-3xl text-base text-white font-bold lg:font-extrabold uppercase text-center lg:-mt-24 -mt-10">
+              Beach
+            </h3>
+          </SwiperSlide>
+        </Swiper>{" "}
+      </section>
     </div>
   );
 };
